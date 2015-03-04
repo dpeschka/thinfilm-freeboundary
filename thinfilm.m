899b8ff44870a3a6480ce719d45cddfa8abdd6f4
dpeschka <peschka@DMBP.local>
2015-03-04 17:28:53 +0100

    Modified thinfilm.m to make it more general and easier to experiment with.

--- thinfilm.m
+++ thinfilm.m
@@ -4,21 +4,21 @@
 clear all
 
 % * set model & computational paramters
-L     =  1.0;  % initial domain size (0,L)
-T     =  1.0;  % final time
-SL    =  1.0;  % negative spreading coefficient at x=x-
-SR    =  1.0;  % negative spreading coefficient at x=x+
-c1    =  0.0;  % normal gravity
-c2    =  0.0;  % tangential gravity
-nt    =  200;  % number of time steps
-npoint=  100;  % number of vertices
+L     = 1.0;  % initial domain size (0,L)
+T     = 0.2;  % final time
+SL    = 1.0;  % negative spreading coefficient at x=x-
+SR    = 1.0;  % negative spreading coefficient at x=x+
+c1    = 0.0;  % normal gravity
+c2    = 0.0;  % tangential gravity
+nt    = 100;  % number of time steps
+npoint= 100;  % number of vertices
 
 % * create element decomposition for FE method
-x                = linspace(0,L,npoint)';
-nelement         = npoint-1;
-nd(1:nelement,1) = 1:npoint-1;
-nd(1:nelement,2) = 2:npoint;
-local_mass_p1    = [1/3 1/6;1/6 1/3];
+x               =linspace(0,L,npoint)';% vertices
+nelement        =npoint-1;             % no elements
+nd(1:nelement,1)=1:npoint-1;           % id left point of an element
+nd(1:nelement,2)=2:npoint;             % id right point of an element
+local_mass_p1   =[1/3 1/6;1/6 1/3];    % mass matrix for reference [0,1]
 
 % * create & remember initial data
 h  = L/2-abs(L/2-x); 
@@ -26,19 +26,11 @@ hi = h;xi = x;
 t  = 0;dt = T/nt;
 
 % * exact stationary solution
-a  = sqrt(sqrt(18)/16);
-xs = linspace(-a,a,100);
-hs = a/sqrt(2)*(1-(xs/a).^2);xs=xs+1/2;
+hold off
+plot(xi,hi,'r--');
+xlabel('x');ylabel('h');
+hold on
 for it=1:nt       
-    % * plot numerical solution with initial data and 
-    % stationary solution for provided data
-    if mod(it,10)==1
-        plot(x,h);
-        xlabel('x'); 
-        ylabel('h');
-        hold on
-        drawnow
-    end
     % * construct system matrices
     build_FE_matrices % script: matrices A,S,Ms,Dx for FEM
     build_ALE_matrix  % script: matrix for ALE decomposition
@@ -53,6 +45,13 @@ for it=1:nt
     udec  = P\hdot(1:ndof); % decompose hdot
     h(2:ndof-1)=h(2:ndof-1)+dt*udec(2:ndof-1);% update h
     x          =x          +dt*I*udec;        % update x
-
+    
+    % * plot numerical solution with initial data and 
+    % stationary solution for provided data
+    if mod(it,10)==1
+        plot(x,h,'b-');
+        drawnow
+    end
 end
+legend('initial','t > 0')
 hold off
